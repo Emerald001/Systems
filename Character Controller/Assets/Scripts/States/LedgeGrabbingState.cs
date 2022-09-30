d8cba9758ea418a7a61854bb7de4b45a71bde519
Andries <an3s01@hotmail.nl>
2022-09-30 15:08:14 +0200

    climbing

--- Character Controller/Assets/Scripts/States/LedgeGrabbingState.cs
+++ Character Controller/Assets/Scripts/States/LedgeGrabbingState.cs
@@ -8,11 +8,20 @@ public class LedgeGrabbingState : MoveState {
     }
 
     public GameObject Ledge;
+    public Vector3 AlongEdge;
 
     public override void OnEnter() {
+        owner.velocity = Vector3.zero;
+
         owner.animator.SetBool("HangingFromEdge", true);
 
         Ledge = owner.evaluator.CanGrabLedge();
+
+        Ray rayOne = new(owner.controller.transform.position + new Vector3(0, 2.2f, 0), owner.transform.forward);
+        if(Physics.Raycast(rayOne, out var hit, 1f, owner.EdgeLayer)) {
+            AlongEdge = hit.normal;
+            AlongEdge = Vector3.Cross(AlongEdge, Vector3.up);
+        }
     }
 
     public override void OnExit() {
@@ -20,16 +29,32 @@ public class LedgeGrabbingState : MoveState {
     }
 
     public override void OnUpdate() {
-        owner.velocity = Vector3.zero;
+        var velocity = Vector3.zero;
 
         var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
 
+        var movedir = Ledge.transform.TransformDirection(input.normalized);
+        velocity += -movedir;
 
+        if (input.x > 0) {
+            owner.animator.SetBool("EdgeWalkRight", true);
+            owner.animator.SetBool("EdgeWalkLeft", false);
+        }
+        else if (input.x < 0) {
+            owner.animator.SetBool("EdgeWalkRight", false);
+            owner.animator.SetBool("EdgeWalkLeft", true);
+        }
+        else {
+            owner.animator.SetBool("EdgeWalkRight", false);
+            owner.animator.SetBool("EdgeWalkLeft", false);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             owner.animator.SetTrigger("Jump");
             owner.velocity += new Vector3(0, Mathf.Sqrt(owner.jumpHeight * -2 * owner.gravity), 0);
         }
+
+        owner.velocity = velocity;
 
         base.OnUpdate();
     }
