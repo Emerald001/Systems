a6e9b5bff20eefd2785ffe13312fb59cb19a9d88
Andries <an3s01@hotmail.nl>
2022-10-05 16:06:34 +0200

    Edited the animations

---
+++ Character Controller/Assets/Scripts/States/GetUpOnPlatformState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetUpOnPlatformState : MoveState {
+
+    public GetUpOnPlatformState(StateMachine<MovementManager> owner) : base(owner) {
+
+    }
+
+    public override void OnEnter() {
+
+    }
+
+    public override void OnExit() {
+
+    }
+
+    public override void OnUpdate() {
+        
+        
+        base.OnUpdate();
+    }
+}
